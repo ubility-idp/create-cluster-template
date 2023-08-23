3cd0318387e04b83f42b38cb929b1c6035c5f192
Ibrahim Nasri <ibrahimnasri24@ubilityai.com>
2023-08-23 12:26:08 +0300

    added the azure cred vars

--- skeleton/variables.tf
+++ skeleton/variables.tf
@@ -1,3 +1,22 @@
+variable "CLIENT_ID" {
+  type      = string
+  sensitive = true
+}
+
+variable "SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "SUBSCRIPTION_ID" {
+  type      = string
+  sensitive = true
+}
+variable "TENANT_ID" {
+  type      = string
+  sensitive = true
+}
+
 variable "resource_group_name" {
   type    = string
   default = "${{ values.resource_group }}"
